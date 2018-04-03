2503d7f55876c247cda9849a8425f82f6f846f1c
stopic13 <sara.topic.18@dartmouth.edu>
2018-04-03 15:42:14 -0400

    Delete method

--- linkedList.cpp
+++ linkedList.cpp
@@ -19,7 +19,7 @@ class LinkedList{
 
 
   void addFirst(Node *node);
-
+  bool deleteNode(int Nodedata);
 
   void printList();
 };
@@ -30,7 +30,7 @@ void LinkedList::printList(){
   while (curr_node != nullptr)
   {
     cout << curr_node-> data;
-    cout << "-------->";
+    cout << "->";
     curr_node = curr_node->next;
   }
 }
@@ -48,6 +48,30 @@ void LinkedList::addFirst(Node *node){
   }
 //  node.next = nullptr;
 }
+
+bool LinkedList::deleteNode(int Nodedata){
+  cout << "called the delete method" << endl;
+  Node *curr_node = head;
+  while (curr_node != nullptr)
+  {
+    if (curr_node->next != nullptr && curr_node->next->data == Nodedata)
+    {
+      cout <<"want to delete"<<endl;
+      cout <<"all good"<<endl;
+      curr_node->next = curr_node->next->next;
+      return true;
+    }
+    else
+    {
+      cout <<"here1"<<endl;
+    }
+    curr_node = curr_node->next;
+  }
+  cout <<"here2"<<endl;
+
+  return false;
+}
+
 
 int main(int argc, char** argv)
 {
@@ -84,6 +108,18 @@ int main(int argc, char** argv)
 
     myLinkedList.printList();
 
+    myLinkedList.deleteNode(2);
+
+    myLinkedList.printList();
+
+    myLinkedList.deleteNode(1);
+
+    myLinkedList.printList();
+
+    std::cout << "---------------" << endl;
+    myLinkedList.deleteNode(4);
+
+    myLinkedList.printList();
 
     // std::cout << myNode.next;
     return 0;
